e8d7a2dd824138272f07b867509683fe78385e57
bopche <sharyubopche2@gmail.com>
2024-09-11 01:59:10 +0530

    code is changed

--- Js/mission.js
+++ Js/mission.js
@@ -14,3 +14,18 @@ counts.forEach((counter) =>{
     }
     upData()
 })
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const slider = document.querySelector('.slider');
+
+left.addEventListener('click',()=>{
+    console.log('Hello');
+});
+<p>
+SpaceX believes a fully and rapidly reusable rocket is the pivotal<br> breakthrough needed to substantially reduce the cost of space<br> access. The majority of the launch cost comes from building the<br> rocket, which historically has flown only once.<br>
+<br>
+Compare that to a commercial airliner – each new plane costs about <br>the same as Falcon 9 but can fly multiple times per day and conduct<br> tens of thousands of flights over its lifetime. Following the <br>commercial model, a rapidly reusable space launch vehicle could <br>reduce the cost of traveling to space by a hundredfold.<br>
+<br>
+
+While most rockets are designed to burn up on reentry, SpaceX<br> rockets can not only withstand reentry but can also successfully land <br>back on Earth and refly again.</p>
